2a20dbb35b3f194717e872dfaad1ebde082de371
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-03-17 15:24:58 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,6 +4,13 @@ def introduction(name)
   name = "Brandon"
 end
 
+def  introduction_with_language(name,language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  name = "brandon"
+  language = "Ruby"
+end
+
+
 
 
 
